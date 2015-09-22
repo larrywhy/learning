159c029e88064bce639834e5618b5b8fbcff5748
larrywhy <larrywhy.tw@gmail.com>
2015-09-22 13:53:17 +0800

    Add gsub! method to substring.rb

--- ruby/substring.rb
+++ ruby/substring.rb
@@ -6,5 +6,8 @@ print "please enter string:"
 user_input = gets.chomp
 if user_input.include? "hello"
     print "yes"
+    # gsub! method
+    user_input.gsub!(/hello/, "world")
 end
+puts user_input
 
